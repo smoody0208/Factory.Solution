1960715c7ccc12b852a5e62cd680b50a7e5a3806
Spencer Moody <spencer.moody@outlook.com>
2020-08-07 09:08:03 -0700

    Add code to Factory Context

---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class FactoryContext : DbContext
+  {
+    public DbSet<Engineer> Engineers { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Machine> Machines { get; set; }
+    public DbSet<EngineerLocationMachine> EngineerLocationMachine { get; set; }
+
+    public FactoryContext(DbContextOptions options) : base(options) { }
+  }
+}  
