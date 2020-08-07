b12ab0ee493f960cb3a1a9719b50898f40efa664
Spencer Moody <spencer.moody@outlook.com>
2020-08-07 09:29:33 -0700

    Create EngineersController and constructor

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using RecordCollection.Models;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controllers
+  {
+    private readonly FactoryContext _db;
+    
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+  }
+}
